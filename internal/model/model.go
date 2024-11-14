ef5b3575d491d6827220c5af005df1cfe4a173f0
DuckBroApprentice <pigbirthin0320@gmail.com>
2024-11-14 16:07:20 +0800

    增加註解

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,5 +1,13 @@
 package model
 
+import (
+	"fmt"
+
+	"github.com/go-programming-tour-book/blog-service/global"
+	"github.com/go-programming-tour-book/blog-service/pkg/setting"
+	"github.com/jinzhu/gorm"
+)
+
 type Model struct {
 	ID         uint32 `gorm:"primary_key" json:"id"`
 	CreatedBy  string `json:"created_by"`
@@ -8,4 +16,32 @@ type Model struct {
 	ModifiedOn uint32 `json:"modified_on"`
 	DeletedOn  uint32 `json:"deleted_on"`
 	IsDel      uint8  `json:"is_del"`
+}
+
+/*
+針對建立DB實例的NewDBEngine方法，同時增加了gorm開放原始碼函數庫的引用和
+MySQL驅動函數庫github.com/jinzhu/gorm/dialects/mysql的初始化
+註：不同類型的DBType需要引用不同的驅動函數庫
+*/
+func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
+	s := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=%s&parseTime=%t&loc=Local")
+	db, err := gorm.Open(databaseSetting.DBType, s,
+		databaseSetting.UserName,
+		databaseSetting.Password,
+		databaseSetting.Host,
+		databaseSetting.DBName,
+		databaseSetting.Charset,
+		databaseSetting.ParseTime,
+	)
+	if err != nil {
+		return nil, err
+	}
+	if global.ServerSetting.RunMode == "debug" {
+		db.LogMode(true)
+	}
+	db.SingularTable(true)
+	db.DB().SetMaxIdleConns(databaseSetting.MaxIdleConns)
+	db.DB().SetMaxIdleConns(databaseSetting.MaxOpenConns)
+
+	return db, nil
 }
