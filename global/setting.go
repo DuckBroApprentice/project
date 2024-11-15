f099f9ee9fd260455935919d576a25b24cdbde77
DuckBroApprentice <pigbirthin0320@gmail.com>
2024-11-15 13:34:38 +0800

    新增記錄檔標準化全域變數

--- global/setting.go
+++ global/setting.go
@@ -1,11 +1,16 @@
 package global
 
-import "github.com/go-programming-tour-book/blog-service/pkg/setting"
+import (
+	"github.com/go-programming-tour-book/blog-service/pkg/logger"
+	"github.com/go-programming-tour-book/blog-service/pkg/setting"
+)
 
-//對最初預估的三個區段進行了設定並宣告了全域變數
-//註：全域變數的初始化會隨著應用程式的不斷演進而改變
+// 對最初預估的三個區段進行了設定並宣告了全域變數
+// 註：全域變數的初始化會隨著應用程式的不斷演進而改變
 var (
 	ServerSetting   *setting.ServerSettingS
 	AppSetting      *setting.AppSettingS
 	DatabaseSetting *setting.DatabaseSettingS
+	//記錄檔標準化:全域變數
+	Logger *logger.Logger
 )
